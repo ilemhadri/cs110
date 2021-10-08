0d0f9f94f7f5d46de7c8694a8538698f3c114b3c
Ismael Lemhadri <19997801+ilemhadri@users.noreply.github.com>
2021-10-07 23:33:43 -0700

    implement inode layer and file layer

--- assigns/assign2/file.c
+++ assigns/assign2/file.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
-#include <assert.h>
 
 #include "file.h"
 #include "inode.h"
 #include "diskimg.h"
 
-// remove the placeholder implementation and replace with your own
-int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
-  fprintf(stderr, "file_getblock(inumber = %d, blockNum = %d) unimplemented. returning -1\n", inumber, blockNum);
-  return -1;  
+int file_getblock(struct unixfilesystem *fs, int inumber, int fileBlockIndex, void *buf) {
+    struct inode inp;
+
+    // fetch inode from inumber
+    if (inode_iget(fs, inumber, &inp) == -1){
+      fprintf(stderr, "inode_iget failed in file_getblock\n");
+      return -1;
+    }
+
+    // convert fileBlockIndex into actual block number
+    int blockNumber = inode_indexlookup(fs, &inp, fileBlockIndex);
+    if (blockNumber == -1){
+      fprintf(stderr,"inode_indexlookup failed in file_getblock\n");
+      return -1;
+    }
+
+    // read block number into buf
+    if (diskimg_readsector(fs->dfd, blockNumber, buf) == -1){
+      fprintf(stderr, "readsector failed in file_getblock\n");
+      return -1;
+    }
+
+    // determine block file size
+    int filesize = inode_getsize(&inp);
+    // return 0 for empty file;
+    if (filesize == 0) return 0;
+    // return 512 if file size is a multiple of 512
+    if (filesize % DISKIMG_SECTOR_SIZE == 0) return DISKIMG_SECTOR_SIZE;
+    // return 512 except if the block is the very last payload block
+    if (fileBlockIndex < filesize / DISKIMG_SECTOR_SIZE) return DISKIMG_SECTOR_SIZE;
+    return filesize % DISKIMG_SECTOR_SIZE;
+
+   return -1; 
 }
--- assigns/assign2/inode.c
+++ assigns/assign2/inode.c
@@ -1,19 +1,73 @@
 #include <stdio.h>
-#include <assert.h>
-
 #include "inode.h"
 #include "diskimg.h"
 
-// remove the placeholder implementation and replace with your own
+#define INODES_PER_BLOCK 16
+#define NUM_BLOCK_NUMS_PER_BLOCK 256
+#define LAST_IADDR 7
+
 int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
-  fprintf(stderr, "inode_get(inumber=%d) not implemented, returning -1\n", inumber);
-  return -1;  
+  int sectorQuotient = (inumber - ROOT_INUMBER) / INODES_PER_BLOCK;
+  int sectorRemainder = (inumber - ROOT_INUMBER) % INODES_PER_BLOCK;
+  struct inode inodesFromSector[INODES_PER_BLOCK];
+  int read = diskimg_readsector(fs->dfd, sectorQuotient + INODE_START_SECTOR, &inodesFromSector);
+  if (read == -1) {
+      fprintf(stderr, "inode_iget can't read sector %d\n", sectorQuotient);
+      return -1;
+  }
+  *inp = inodesFromSector[sectorRemainder];
+  return 0;
 }
 
-// remove the placeholder implementation and replace with your own
-int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {
-  fprintf(stderr, "inode_indexlookup(blockNum=%d) not implemented, returning -1\n", blockNum);
-  return -1;
+int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int fileBlockIndex) {
+    /* inode not allocated; do nothing */
+    if (!(inp->i_mode & IALLOC)){
+        fprintf(stderr, "inode unallocated. inode_indexlookup cannot proceed."); 
+        return -1;
+    }
+
+    /* deal with direct addressing first */
+    if ((inp->i_mode & ILARG) == 0){
+        if (fileBlockIndex > LAST_IADDR) {
+           fprintf(stderr, "fileBlockIndex exceeds number of direct blocks available");
+           return -1;
+       }
+       return inp->i_addr[fileBlockIndex];
+    }
+
+    /* deal with indirect addressing */
+    uint16_t singlyIndirect, direct;
+    /* determine whether singly or doubly indirect */
+    int fileBlockIndexSub = fileBlockIndex - LAST_IADDR * NUM_BLOCK_NUMS_PER_BLOCK;
+
+    /* process doubly indirect addressing */
+    if (fileBlockIndexSub >= 0){
+        uint16_t doublyIndirectBlocks[NUM_BLOCK_NUMS_PER_BLOCK];
+        if (diskimg_readsector(fs->dfd, inp->i_addr[LAST_IADDR], &doublyIndirectBlocks) == -1) {
+            fprintf(stderr, "could not read singly indirect blocks from doubly indirect");
+            return -1;
+        }
+        uint16_t doublyIndirect = fileBlockIndexSub / NUM_BLOCK_NUMS_PER_BLOCK;
+
+        singlyIndirect = doublyIndirectBlocks[doublyIndirect];
+        direct = fileBlockIndexSub % NUM_BLOCK_NUMS_PER_BLOCK;
+    } 
+    /* process singly indirect addressing */
+    else {
+        uint16_t fbiQuotient = fileBlockIndex / NUM_BLOCK_NUMS_PER_BLOCK;
+        uint16_t fbiRemainder = fileBlockIndex % NUM_BLOCK_NUMS_PER_BLOCK;
+
+        singlyIndirect = inp->i_addr[fbiQuotient];
+        direct = fbiRemainder;
+    }
+
+    /* read direct block */
+    uint16_t directBlocks[NUM_BLOCK_NUMS_PER_BLOCK];
+    if (diskimg_readsector(fs->dfd, singlyIndirect, &directBlocks) == -1) {
+        fprintf(stderr, "could not read direct blocks from singly indirect");
+        return -1;
+    }
+    return directBlocks[direct];
 }
 
 int inode_getsize(struct inode *inp) {
